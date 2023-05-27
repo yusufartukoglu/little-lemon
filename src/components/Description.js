fd1f030b67b32d4dcbd83fc90fa89867ec20bd3c
Yusuf Artukoğlu <yusufartukoglu@gmail.com>
2023-05-27 20:58:18 +0300

    Fixed some style bugs

--- src/components/Description.js
+++ src/components/Description.js
@@ -7,10 +7,12 @@ const Description = () => {
       <div className="about-content">
       <div className="little desc-title">Little Lemon</div>
       <div className="subtitle desc-subtitle">Chicago</div>
-      <p className="desc-text desc">
+      <div className="desc-pharagraph">
+        <p className="desc-text desc">
       Based in Chicago, Illinois, Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day.<br/><br/>
             Little Lemon is owned by two Italian brothers, Mario and Adrian, who moved to the United States to pursue their shared dream of owning a restaurant. To craft the menu, Mario relies on family recipes and his experience as a chef in Italy. Adrian does all the marketing for the restaurant and led the effort to expand the menu beyond classic Italian to incorporate additional cuisines from the Mediterranean region.
       </p>
+      </div>
       </div>
       <div className="owners">
       <img className="chefs" src={Owners} alt="Mario and Adrian"></img>
